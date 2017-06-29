d3cc2ec445ef274bcc088696e231c04ad6688d52
Trisatan-xiaoxianghu <hxxjesse@163.com>
2017-06-29 15:52:41 +0200

    Signed-off-by: Trisatan-xiaoxianghu <hxxjesse@163.com>

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -10,26 +10,25 @@ public class LoginServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {		
-//		String userName = request.getParameter("userName");
-//		String userPwd = request.getParameter("userPwd");
-//		if("tristan".equals(userName)
-//				 && "123456".equals(userPwd)){
-//			HttpSession session = request.getSession();
-//			session.setAttribute("loginName", userName);
-//			response.sendRedirect(request.getContextPath()+"/IndexServlet");
-//			
-//		}else{
-//		
-//			response.sendRedirect(request.getContextPath()+"/fail.html");
+		String userName = request.getParameter("userName");
+		String userPwd = request.getParameter("userPwd");
+		if("tristan".equals(userName)
+				 && "123456".equals(userPwd)){
+			HttpSession session = request.getSession();
+			session.setAttribute("loginName", userName);
+			response.sendRedirect(request.getContextPath()+"/IndexServlet");
+			
+		}else{
+			response.sendRedirect(request.getContextPath()+"/fail.html");
 		
-//		}
-		response.getWriter().write("i am coming");
+		}
+		
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-//		doGet(request, response);
-		response.getWriter().write("i am coming");
+			doGet(request, response);
+		
 	}
 
 }
